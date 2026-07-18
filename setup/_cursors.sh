bbafac4e34548657c762203dcd116b97d5731554
salar <88825426+salarmotevalli@users.noreply.github.com>
2026-07-19 00:43:17 +0330

    Update _cursors.shchange wget to curl

--- setup/_cursors.sh
+++ setup/_cursors.sh
@@ -6,14 +6,15 @@
 download_folder="$HOME/Downloads/bibata-cursors"
 bibata_url="https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/"
 
-if [ -d $download_folder ]; then
-    rm -rf $download_folder
+if [ -d "$download_folder" ]; then
+    rm -rf "$download_folder"
 fi
-mkdir -p $download_folder
+mkdir -p "$download_folder"
 
-wget -P $download_folder $bibata_url/Bibata-Modern-Amber.tar.xz
-wget -P $download_folder $bibata_url/Bibata-Modern-Classic.tar.xz
-wget -P $download_folder $bibata_url/Bibata-Modern-Ice.tar.xz
+# Download using curl with -L to follow redirects and -o to specify output path
+curl -L -o "$download_folder/Bibata-Modern-Amber.tar.xz" "$bibata_url/Bibata-Modern-Amber.tar.xz"
+curl -L -o "$download_folder/Bibata-Modern-Classic.tar.xz" "$bibata_url/Bibata-Modern-Classic.tar.xz"
+curl -L -o "$download_folder/Bibata-Modern-Ice.tar.xz" "$bibata_url/Bibata-Modern-Ice.tar.xz"
 
 if [ ! -d ~/.local/share/icons/ ]; then
     mkdir -p ~/.local/share/icons/
@@ -25,14 +26,14 @@ fi
 if [ -d ~/.local/share/icons/Bibata-Modern-Classic ]; then
     rm -rf ~/.local/share/icons/Bibata-Modern-Classic
 fi
-if [ -d ~/.local/share/icons/Bibata-Modern-Amber ]; then
+if [ -d ~/.local/share/icons/Bibata-Modern-Ice ]; then  # Fixed: was incorrectly checking Amber again
     rm -rf ~/.local/share/icons/Bibata-Modern-Ice
 fi
 
-tar -xf $download_folder/Bibata-Modern-Amber.tar.xz -C ~/.local/share/icons/
-tar -xf $download_folder/Bibata-Modern-Classic.tar.xz -C ~/.local/share/icons/
-tar -xf $download_folder/Bibata-Modern-Ice.tar.xz -C ~/.local/share/icons/
+tar -xf "$download_folder/Bibata-Modern-Amber.tar.xz" -C ~/.local/share/icons/
+tar -xf "$download_folder/Bibata-Modern-Classic.tar.xz" -C ~/.local/share/icons/
+tar -xf "$download_folder/Bibata-Modern-Ice.tar.xz" -C ~/.local/share/icons/
 
-if [ -d $download_folder ]; then
-    rm -rf $download_folder
+if [ -d "$download_folder" ]; then
+    rm -rf "$download_folder"
 fi
